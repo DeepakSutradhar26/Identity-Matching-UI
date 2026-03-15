06dc5b1fa26c089049918a9e4f925a04932e0263
Deepak Sutradhar <140784188+DeepakSutradhar26@users.noreply.github.com>
2026-03-15 16:01:00 +0530

    frontend

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [result, setResult] = useState<any>(null);
+
+  const handleSubmit = async() => {
+    const res = await fetch("https://identity-matching-api-jv8j.vercel.app/identify", {
+      method : "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email || null,
+        phoneNumber: phone || null,
+      }),
+    });
+
+    const data = await res.json();
+    setResult(data);
+  };
+
+  return <div style = {{padding : 30}}>
+    <h1>Indentity Matching API</h1>
+
+    <input
+      placeholder='Email'
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+    >
+    </input>
+    <input
+      placeholder='Phone Number'
+      value={phone}
+      onChange={(e) => setPhone(e.target.value)}
+    >
+    </input>
+
+    <button onClick={handleSubmit}> Submit </button>
+
+    {result && (<pre>{JSON.stringify(result, null, 2)}</pre>)}
+
+  </div>;
+}
+
+export default App
